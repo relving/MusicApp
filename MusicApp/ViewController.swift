9f93e29c115cd25b7c67c98ca17550146325be27
Lena Azmi <bibleofil@gmail.com>
2019-02-28 13:51:33 +0300

    Initial Commit

---
+++ MusicApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicApp
+//
+//  Created by Lena Azmi on 28/02/2019.
+//  Copyright © 2019 Lena Azmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
